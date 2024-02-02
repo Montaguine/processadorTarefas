2d76dd1466d24a1a83b5e34da8ef16f1c0a2d979
Montaguine <yuria.cifuentes@gmail.com>
2024-02-02 12:20:46 -0300

    feat: Implementou Repository

---
+++ ProcessadorTarefas/Repositorios/Repository.cs
@@ -0,0 +1,30 @@
+﻿using ProcessadorTarefas.Entidades;
+using SOLID_Example.Interfaces;
+
+namespace ProcessadorTarefas.Repositorios
+{
+    internal class Repository : IRepository<Tarefa>
+    {
+        static IEnumerable<Tarefa> repository = new List<Tarefa>();
+        public void Add(Tarefa entity)
+        {
+            repository.Append(entity);
+        }
+        public IEnumerable<Tarefa> GetAll()
+        {
+            return repository;
+        }
+        public Tarefa? GetById(int id)
+        {
+            return repository.FirstOrDefault(x => x.Id == id);
+        }
+        public void Update(Tarefa entity)
+        {
+            if (int.TryParse(Console.ReadLine(), out int estado))
+            {
+                entity.Estado = (EstadoTarefa)estado;
+            }
+        }
+
+    }
+}
